405ee64a1f60223ef0dd30754733eee15b9ccf22
Priyankgupt <67186956+Priyankgupt@users.noreply.github.com>
2020-10-16 17:47:14 +0530

    Add files via upload

---
+++ patt6.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+  int i,j,n,k;
+  printf("enter your limit");
+  scanf("%d",&n);
+  for(i=1;i<=n;i++)
+  {
+      for(j=1;j<=i-1;j++)
+      {
+          printf(" ");
+
+      }
+      for(k=1;k<=2*(n-i)+1;k++)
+      {
+          printf("*");
+      }
+        printf("\n");
+  }
+}
+
